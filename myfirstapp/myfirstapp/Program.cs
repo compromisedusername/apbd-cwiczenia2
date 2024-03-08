05347503b2dab7fe85db102a5f31e1d56d6c9e25
s28076 <haldman123@gmail.com>
2024-03-08 13:42:06 +0100

    add GetAverage

--- myfirstapp/myfirstapp/Program.cs
+++ myfirstapp/myfirstapp/Program.cs
@@ -3,3 +3,14 @@
 Console.WriteLine("Goodbye World!");
 Console.WriteLine("And Hello World!");
 Console.WriteLine("And Again Hello World!");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double) sum / numbers.Length;
+}
